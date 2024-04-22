2f057bbdd76a3df64a5b1b11d8b23dc2ffb6263e
jaderaposa <jaduya678@gmail.com>
2024-04-22 17:39:19 +0800

    Refactor search bar UI in mentee_home.dart

--- lib/pages/mentee_home.dart
+++ lib/pages/mentee_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentor_shift/objects/style/boxshadow.dart';
 // import 'package:mentor_shift/objects/style/paddedcontainer.dart';
 
 class MenteeHome extends StatefulWidget {
@@ -9,6 +10,8 @@ class MenteeHome extends StatefulWidget {
 }
 
 class MenteeHomeState extends State<MenteeHome> {
+  String dropdownValue = 'NAME';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +31,11 @@ class MenteeHomeState extends State<MenteeHome> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
             child: Container(
               padding:
-                  const EdgeInsets.symmetric(horizontal: 0.0, vertical: 5.0),
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0.0),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(8),
@@ -42,22 +46,75 @@ class MenteeHomeState extends State<MenteeHome> {
                       blurRadius: 6.0)
                 ],
               ),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Search',
-                  labelStyle: TextStyle(
-                    fontFamily: 'ProtestRiot',
-                    color: Color(0xFF076A89),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 3.0),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300], // set the background color
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: const [kBoxShadow],
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: PopupMenuButton<String>(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                dropdownValue,
+                                style: const TextStyle(
+                                  color: Color(0xFF076A89),
+                                  fontFamily: 'ProtestRiot',
+                                ),
+                              ),
+                              const Icon(Icons.arrow_downward,
+                                  color: Color(0xFF076A89)),
+                            ],
+                          ),
+                          onSelected: (String value) {
+                            setState(() {
+                              dropdownValue = value;
+                            });
+                          },
+                          itemBuilder: (BuildContext context) {
+                            return <String>['NAME', 'EXPERTISE']
+                                .map((String value) {
+                              return PopupMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: const TextStyle(
+                                    fontFamily: 'ProtestRiot',
+                                    color: Color(0xFF076A89),
+                                  ),
+                                ),
+                              );
+                            }).toList();
+                          },
+                        ),
+                      ),
+                    ),
                   ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: InputBorder.none,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 5.0, horizontal: 15.0),
-                ),
-                style: const TextStyle(
-                  fontFamily: 'ProtestRiot',
-                ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText:
+                            'Search Mentors', // change labelText to hintText
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 15.0),
+                      ),
+                      style: const TextStyle(
+                        fontFamily: 'ProtestRiot',
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
